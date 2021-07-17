d6ed487b7f95a980deb7c219c44d302dce538c05
SMRITHI-T <74653719+SMRITHI-T@users.noreply.github.com>
2021-07-17 12:29:20 +0530

    Add files via upload

---
+++ challenge2.js
@@ -0,0 +1,16 @@
+class Challenge2{
+    constructor(){
+        this.heading=createElement('h1');
+        this.security=new Security();
+        
+    }
+    display(){
+        this.heading.html("CHALLENGE 2")
+this.heading.position(600,0);
+clues();
+this.security.display();
+    }
+    if(score){
+
+    }
+}
